4d8d69ca26e5d2fd23527ea3024e1473acca11ec
naymurabir <naymurrahman.abir@gmail.com>
2023-11-01 00:28:35 +0600

    Service added

--- src/Pages/AddServices/AddServices.jsx
+++ src/Pages/AddServices/AddServices.jsx
@@ -1,8 +1,41 @@
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const AddServices = () => {
+
+    const handleAddServices = e => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const title = form.get('title')
+        const type = form.get('type')
+        const img = form.get('img')
+        const price = form.get('price')
+        const description = form.get('description')
+        const newService = { title, type, img, price, description }
+        console.log(newService);
+
+        axios.post('http://localhost:5000/services', newService)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: "A new service has been added successfully.",
+                        showConfirmButton: false,
+                        background: '#343436',
+                        heightAuto: '100px',
+                        color: 'white',
+                        timer: 2000
+                    })
+                    e.target.reset()
+                }
+            })
+    }
+
     return (
         <div>
-            <form >
+            <form onSubmit={handleAddServices}>
                 <div className="bg-gray-100 lg:w-2/4 mx-auto p-5 my-5 rounded">
 
                     <h2 className="text-xl font-bold text-[#FF3811] text-center mb-5">Add New Service</h2>
@@ -12,6 +45,7 @@ const AddServices = () => {
 
                         <input name="type" type="text" placeholder="Type..." className="input input-bordered input-error w-full max-w-xs text-xs" />
                     </div>
+
                     <div>
                         <div className="flex flex-col md:flex-row gap-5 justify-between mt-5">
                             <input name="img" type="text" placeholder="Image..." className="input input-bordered input-error w-full max-w-xs text-xs" />
